62b4568380d9cf33102654b30f4af6d8a63eff83
dupanovic <dupanovicarman@gmail.com>
2024-05-04 00:30:25 +0200

    feat: generate points with script for x in -1 to 1

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -1,18 +1,30 @@
-using System;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class Graph : MonoBehaviour
 {
     [SerializeField]
     Transform pointPrefab;
 
+    [SerializeField, Range(1, 100)]
+    int resolution = 10;
+
     void Awake()
     {
-        Transform point = Instantiate(pointPrefab);
-        point.localPosition = Vector3.right;
+        Transform point;
+        float step = 2f / resolution;
+        Vector3 scale = Vector3.one * step;
+        Vector3 position = Vector3.zero;
+        for (int i = 0; i < resolution; i++)
+        {
+            point = Instantiate(pointPrefab);
+            point.SetParent(transform, false);
 
-        point = Instantiate(pointPrefab);
-        point.localPosition = Vector3.right * 2f;
+            position.x = (i + 0.5f) * step - 1f;
+            position.y = position.x * position.x;
+            point.localPosition = position;
+            point.localScale = scale;
+        }
     }
 
     // Update is called once per frame
