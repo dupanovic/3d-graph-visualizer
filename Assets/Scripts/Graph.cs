d21737b1102abbc61d8c04f4b7bbb87852a61452
dupanovic <dupanovicarman@gmail.com>
2024-05-04 18:42:06 +0200

    feat: write urp shader and animate graph

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -9,19 +9,22 @@ public class Graph : MonoBehaviour
     [SerializeField, Range(1, 100)]
     int resolution = 10;
 
+    Transform[] points;
+
+
     void Awake()
     {
-        Transform point;
         float step = 2f / resolution;
         Vector3 scale = Vector3.one * step;
         Vector3 position = Vector3.zero;
+        points = new Transform[resolution];
         for (int i = 0; i < resolution; i++)
         {
-            point = Instantiate(pointPrefab);
+            Transform point = Instantiate(pointPrefab);
+            points[i] = point;
             point.SetParent(transform, false);
 
             position.x = (i + 0.5f) * step - 1f;
-            position.y = position.x * position.x;
             point.localPosition = position;
             point.localScale = scale;
         }
@@ -30,6 +33,17 @@ public class Graph : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float time = Time.time;
+        for (int i = 0; i < resolution; i++)
+        {
+            Transform point = points[i];
+
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time)) * 2;
+
+            point.localPosition = position;
+
+        }
 
     }
 }
